e3a4ac392c3779d8e927cfd470f347718d6be62d
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-02-10 14:12:53 -0800

    fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 import { TiktokenEncoding } from "tiktoken";
 
 // TODO: Add more models
+// https://github.com/dqbd/tiktoken/blob/74c147e19584a3a1acea0c8e0da4d39415cd33e0/wasm/src/lib.rs#L328
 export const TIKTOKEN_MODEL_MAPPING: Record<string, TiktokenEncoding> = {
   "gpt-4": "cl100k_base",
 };
