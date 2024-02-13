950284d9444eadb48a61f5d2e5aae98896800be6
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-02-13 15:01:10 -0800

    upsert

--- src/instrumentation/pinecone/patch.ts
+++ src/instrumentation/pinecone/patch.ts
@@ -1,4 +1,4 @@
-import { SpanKind, trace } from "@opentelemetry/api";
+import { SpanKind, SpanStatusCode, trace } from "@opentelemetry/api";
 import { SERVICE_PROVIDERS, TRACE_NAMESPACES } from "../../constants";
 import { APIS } from "./lib/apis";
 import { PineconeSpanAttributes } from "./lib/span_attributes";
@@ -10,8 +10,6 @@ export function patchUpsert(
     // Preserving `this` from the calling context
     const originalContext = this;
 
-    console.log(this.target);
-
     // Start a new span
     const span = trace
       .getTracer(TRACE_NAMESPACES.PINECONE)
@@ -22,5 +20,44 @@ export function patchUpsert(
         },
         kind: SpanKind.CLIENT,
       });
+
+    if (this.target?.index) {
+      span.setAttribute(
+        PineconeSpanAttributes.REQUEST_INDEX,
+        this.target?.index
+      );
+    }
+    if (this.target?.namespace) {
+      span.setAttribute(
+        PineconeSpanAttributes.REQUEST_NAMESPACE,
+        this.target?.namespace
+      );
+    }
+    if (this.target?.indexHostUrl) {
+      span.setAttribute(
+        PineconeSpanAttributes.REQUEST_INDEX_HOST_URL,
+        this.target?.indexHostUrl
+      );
+    }
+
+    // Wrap the original method in a try/catch block
+    try {
+      // Call the original create method
+      const response = await originalMethod.apply(originalContext, args);
+
+      // Set the span status and end the span
+      if (response) {
+        span.setAttribute("response", JSON.stringify(response?.data));
+      }
+      span.setStatus({ code: SpanStatusCode.OK });
+      span.end();
+      return response;
+    } catch (error: any) {
+      // If an error occurs, record the exception and end the span
+      span.recordException(error);
+      span.setStatus({ code: SpanStatusCode.ERROR, message: error.message });
+      span.end();
+      throw error;
+    }
   };
 }
