80193ece49d3c39c0103810cc1de9fc87321253d
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-03-26 18:24:07 -0700

    cleanup

--- src/examples/entrypoint.ts
+++ src/examples/entrypoint.ts
@@ -1,10 +1,3 @@
-// import { basic } from '@langtrace-examples/llamaindex/basic'
-// import { basic } from '@langtrace-examples/anthropic/basic'
-// import { chatCompletion } from '@langtrace-examples/openai/chat-completion'
+import { chatCompletion } from '@langtrace-examples/openai/chat-completion'
 
-import { basic } from '@langtrace-examples/chroma/basic'
-
-// void basic()
-// void basic()
-// void chatCompletion()
-void basic()
+void chatCompletion()
