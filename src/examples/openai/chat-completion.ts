f3d8eddbc8abdedce824d6aa3618873baba88116
Karthik Kalyanaraman <karthik@scale3labs.com>
2024-03-26 17:04:47 -0700

    cleanup

--- src/examples/openai/chat-completion.ts
+++ src/examples/openai/chat-completion.ts
@@ -18,32 +18,10 @@ export async function chatCompletion (): Promise<void> {
       model: 'gpt-4',
       messages: [
         { role: 'user', content: 'Say this is a test 3 times' }
-        // {
-        //   role: "assistant",
-        //   content: "This is a test. This is a test. This is a test.",
-        // },
-        // { role: "user", content: "Say this is a mock 4 times" },
       ],
       stream: true
     })
 
-    // console.info(response)
-
-    for await (const part of response) {
-      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions, @typescript-eslint/prefer-nullish-coalescing
-      process.stdout.write(part.choices[0]?.delta?.content || '')
-    }
-
-    // const completion2 = await openai.chat.completions.create({
-    //   model: "gpt-4",
-    //   messages: [
-    //     { role: "user", content: "Say this is a test 3 times" },
-    //     {
-    //       role: "assistant",
-    //       content: "This is a test. This is a test. This is a test.",
-    //     },
-    //     { role: "user", content: "Say this is a mock 4 times" },
-    //   ],
-    // });
+    console.info(response)
   })
 }
