a38229d87cace968c63cab13e2ee2d14161e283d
Rohit Kadhe <rohit@scale3labs.com>
2024-03-27 09:41:06 -0600

    fixes

--- src/examples/pinecone/basic.ts
+++ src/examples/pinecone/basic.ts
@@ -8,7 +8,7 @@ dotenv.config()
 
 const pc = new Pinecone()
 const openai = new OpenAI()
-init({ batch: false, write_to_remote_url: false })
+init({ batch: false, write_to_langtrace_cloud: false })
 
 export async function basic (): Promise<void> {
   await withLangTraceRootSpan(async () => {
