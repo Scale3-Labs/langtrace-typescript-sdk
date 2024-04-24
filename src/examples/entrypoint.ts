6d9d5f06c7ad85d9d4c288f2bd1661da0e5e1414
Rohit Kadhe <rohit@scale3labs.com>
2024-04-24 08:48:01 -0600

    undo example changes

--- src/examples/entrypoint.ts
+++ src/examples/entrypoint.ts
@@ -1,11 +1,3 @@
 
-// import { chatCompletionStreaming } from '@langtrace-examples/openai/chat-completion-streaming'
 import { basicChat } from '@langtrace-examples/cohere/basic'
-// void basic()
 void basicChat()
-
-// void chatCompletionStreaming()
-// void basicRerank()
-// void basicAgent()
-// void functionCalling()
-// void embeddingsCreate()
