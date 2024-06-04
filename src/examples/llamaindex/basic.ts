b951e77baf04995e4fe015ce791958acff189810
Rohit Kadhe <rohit@scale3labs.com>
2024-06-04 10:44:22 -0400

    shorten example

--- src/examples/llamaindex/basic.ts
+++ src/examples/llamaindex/basic.ts
@@ -1,4 +1,5 @@
 import { init } from '@langtrace-init/init'
+import { withLangTraceRootSpan } from '@langtrace-utils/instrumentation'
 import dotenv from 'dotenv'
 import type { BaseReader, Metadata } from 'llamaindex'
 import {
@@ -20,24 +21,19 @@ init({
 })
 
 export async function basic (): Promise<void> {
-  // await withLangTraceRootSpan(async (spanId, traceId) => {
-  // Load essay from abramov.txt in Node
-  // const path = 'node_modules/llamaindex/examples/abramov.txt'
-
-  // const essay = await fs.readFile(path, 'utf-8')
-
+  await withLangTraceRootSpan(async (spanId, traceId) => {
   // // Create Document object with essay
-  const document = new Document({ text: "Author of 'React' and 'Redux' books. He is a software engineer at Facebook. He studied at the University of Moscow and studied engineering" })
+    const document = new Document({ text: "Author of 'React' and 'Redux' books. He is a software engineer at Facebook. He studied at the University of Moscow and studied engineering" })
 
-  // // Split text and create embeddings. Store them in a VectorStoreIndex
-  const index = await VectorStoreIndex.fromDocuments([document])
+    // // Split text and create embeddings. Store them in a VectorStoreIndex
+    const index = await VectorStoreIndex.fromDocuments([document])
 
-  // Query the index
-  const queryEngine = index.asQueryEngine()
-  const response = await queryEngine.query({ query: 'What did the author do in college?' })
-  // Output response
-  console.info(response.toString())
-  // })
+    // Query the index
+    const queryEngine = index.asQueryEngine()
+    const response = await queryEngine.query({ query: 'What did the author do in college?' })
+    // Output response
+    console.info(response.toString())
+  })
 }
 
 export async function extractor (): Promise<void> {
