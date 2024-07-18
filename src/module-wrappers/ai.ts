0eff373816109dd877480e9b9b3824bb6aa3f158
Rohit Kadhe <rohit@scale3labs.com>
2024-07-18 17:31:22 -0600

    add support for commonjs import which was missing

--- src/module-wrappers/ai.ts
+++ src/module-wrappers/ai.ts
@@ -3,5 +3,7 @@ const originalModule = require('ai')
 // Create a wrapper object. This is necessary because the original module has properties that are read-only
 const ai = Object.assign({}, originalModule)
 
-// Export the wrapper object
+// Support commonjs import
+module.exports = ai
+// Support es6 import
 export default ai
