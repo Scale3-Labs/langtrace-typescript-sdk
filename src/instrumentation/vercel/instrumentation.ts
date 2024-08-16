d04db8e20033125d4ea453c128c3e0522cb5ca21
Darshit Suratwala <darshit@scale3labs.com>
2024-08-16 13:53:13 +0530

    feat: adding vercel-ai anthropic support

--- src/instrumentation/vercel/instrumentation.ts
+++ src/instrumentation/vercel/instrumentation.ts
@@ -1,9 +1,9 @@
 import { diag } from '@opentelemetry/api'
 import { InstrumentationBase, InstrumentationModuleDefinition, InstrumentationNodeModuleDefinition } from '@opentelemetry/instrumentation'
 /* eslint-disable no-restricted-imports */
-import { version, name } from '../../../package.json'
+import { embedPatch, generateTextPatch, streamTextPatch, generateObjectPatch, streamObjectPatch } from '@langtrace-instrumentation/vercel/patch'
 import { APIS } from '@langtrase/trace-attributes'
-import { embedPatch, generateTextPatch, streamTextPatch } from '@langtrace-instrumentation/vercel/patch'
+import { name, version } from '../../../package.json'
 
 class VercelAIInstrumentation extends InstrumentationBase<any> {
   constructor () {
@@ -38,6 +38,8 @@ class VercelAIInstrumentation extends InstrumentationBase<any> {
     this._wrap(moduleExports, 'streamText', (original: any) => streamTextPatch.call(this, original as (...args: any[]) => any, APIS.ai.STREAM_TEXT.METHOD, this.tracer, version, name, moduleVersion))
     this._wrap(moduleExports, 'embed', (original: any) => embedPatch.call(this, original as (...args: any[]) => any, APIS.ai.EMBED.METHOD, this.tracer, version, name, moduleVersion))
     this._wrap(moduleExports, 'embedMany', (original: any) => embedPatch.call(this, original as (...args: any[]) => any, APIS.ai.EMBED_MANY.METHOD, this.tracer, version, name, moduleVersion))
+    this._wrap(moduleExports, 'generateObject', (original: any) => generateObjectPatch.call(this, original as (...args: any[]) => any, APIS.ai.GENERATE_OBJECT.METHOD, this.tracer, version, name, moduleVersion))
+    this._wrap(moduleExports, 'streamObject', (original: any) => streamObjectPatch.call(this, original as (...args: any[]) => any, APIS.ai.STREAM_OBJECT.METHOD, this.tracer, version, name, moduleVersion))
   }
 
   public _unpatch (module: any): void {
